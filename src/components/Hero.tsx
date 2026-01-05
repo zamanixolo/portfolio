c7eab47dd4506fb5484a2bd04ea47733d4cc66a1
zamanixolo <zamani.xolo@gmail.com>
2026-01-05 19:53:58 +0200

    Feat: Add 10% transparent black overlay over hero video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -99,6 +99,7 @@ export default function Hero() {
                         className="w-full h-full object-cover opacity-50"
                         style={{ width: '100vw', height: 'auto' }}
                     />
+                    <div className="absolute inset-0 bg-black opacity-10 z-10" /> {/* Transparent black overlay */}
                 </div>
             )}
             
